86e88114ee52cd5041e1d7cef18a2b4b6d21d131
LudwigGrandin <grandin.ludwig@live.fr>
2019-12-06 21:23:38 +0100

    Projet Fini

--- main.c
+++ main.c
@@ -11,8 +11,7 @@ void generationIpM(int m, int *n,int *D);
 void generationIR(int *D, int n, int min,int max);
 int LSA(int m, int n, int *D, int *M,int *borneInferieurMoyenne);
 int LTP(int m, int n, int *D, int *M,int *borneInferieurMoyenne);
-void initM(int *M, int n);
-//void calculBorne(int *M, int m, int *borneInferieurMax, int *borneInferieurMoyenne);
+void initM(int *M, int m);
 
 int main()
 {
@@ -21,19 +20,13 @@ int main()
     int continuer = 0, choix = 0;
     //m = nbMachine , n = nombre de tâches
     int m=0,n=0,borneInferieurMoyenne=0,borneInferieurMax=0, nbK = 0, min = 0 , max = 0;
-    int K = 0;
+    int k = 0;
     float borneInfMaxLSA=0,borneInfMaxLTP=0;
-    int tempsTotal;
     float maxb=0, resultatLSA=0 , resultatLTP=0;
     float ratioLSA=0, ratioLTP=0;
-    int tabval[27]={2,3,4,5,6,7,8,9,10,20,30,40,50,60,70,80,90,100,150,200,250,300,350,400,450,500,550}, nbval=27;
-    int i=0, y=0;
-    int nMax;
+    float ratioLSAMoyen=0, ratioLTPMoyen=0;
+    //int tabval[27]={2,3,4,5,6,7,8,9,10,20,30,40,50,60,70,80,90,100,150,200,250,300,350,400,450,500,550}, nbval=27;
     int D[302500]={0}, M[1000]={0};
-
-
-
-    //Boucle pour avoir toutes les valeurs de tabVal
 
  /**DEBUT DE L'ALGORITHME**/
 do
@@ -41,26 +34,21 @@ do
 
     if(choix == 0)
     {
-        menuUtilisateur(&choix,&m,&n,&nbK,&min,&max);
+        menuUtilisateur(&choix,&m,&n,&k,&min,&max);
+        nbK = k;
+        k=0;
     }
     else{}
 
-    /*nMax = m*m+1;
-    int D[nMax], M[n];*/
-
- for(y=0;y<nbval;y++)
-    {
 
 
-    initM(M,n);
-    //m=tabval[y];
+    initM(M,m);
 
     printf("m=%d\n",m);
-    //printf("\n\n les valeurs sont les suivantes :\nm = %d\nn = %d, k = %d\n borneInferieurMoyenne = %d\ borneInferieurMax = %d\n",m,n,k,borneInferieurMoyenne,borneInferieurMax);
+
 
 
 //selon le choix de l'utilisation on ne générera pas la même instance
-
     switch(choix)
     {
         case 1 :
@@ -77,10 +65,10 @@ do
 
     }
 
-
+/**Partie Calculs des valeurs LSA,LTP, bornes et RATIO**/
     resultatLSA = LSA(m,n,D,M,&borneInferieurMoyenne);
     borneInfMaxLSA=resultatLSA;
-    initM(M,n);
+    initM(M,m);
     resultatLTP=LTP(m,n,D,M,&borneInferieurMoyenne);
     //BorneInfMax = Durée la plus grande
     borneInfMaxLTP=resultatLTP;
@@ -89,10 +77,6 @@ do
     {borneInferieurMax = borneInfMaxLSA;}
     else
     {borneInferieurMax = borneInfMaxLTP;}
-
-
-
-
     //Calcul MaxB
     if(borneInferieurMax >= borneInferieurMoyenne)
     {maxb=borneInferieurMax;}
@@ -101,19 +85,38 @@ do
     ratioLSA = resultatLSA / maxb;
     ratioLTP = resultatLTP / maxb;
 
-    //Affichage final
-
-    printf("BorneInferieurMax : %d\n", borneInferieurMax);
-    printf("BorneInferieurMoyenne : %d\n",borneInferieurMoyenne);
-    printf("Resultat LSA : %.0f\n",resultatLSA);
-    printf("maxb : %f\n",maxb);
-    printf("Ratio LSA : %.2f\n",ratioLSA);
-    printf("Resultat LTP : %.0f\n", resultatLTP);
-    printf("Ratio LTP : %.2f\n\n", ratioLTP);
-    }
-    if(nbK != 0)
+    //Affichage final. Affichage classique si on est en instance Im ou I'm. Si On est en instance IR, alors on fait le calcul du ratio moyen LSA et LTP
+    if(choix != 3)
     {
-        nbK --;
+        printf("BorneInferieurMax : %d\n", borneInferieurMax);
+        printf("BorneInferieurMoyenne : %d\n",borneInferieurMoyenne);
+        printf("Resultat LSA : %.0f\n",resultatLSA);
+        printf("maxb : %f\n",maxb);
+        printf("Ratio LSA : %.2f\n",ratioLSA);
+        printf("Resultat LTP : %.0f\n", resultatLTP);
+        printf("Ratio LTP : %.2f\n\n", ratioLTP);
+    }
+    else if (choix == 3)
+    {
+        if(k < nbK)
+        {
+            ratioLSAMoyen += ratioLSA;
+            ratioLTPMoyen += ratioLTP;
+        }
+        else
+        {
+            ratioLSAMoyen /= nbK;
+            ratioLTPMoyen /= nbK;
+            printf("ratioLSAMoyen : %.2f\n", ratioLSAMoyen);
+            printf("ratioLTPMoyen : %.2f\n", ratioLTPMoyen);
+
+        }
+    }
+
+
+    if(k < nbK)
+    {
+        k ++;
         continuer = 1;
     }
     else
@@ -131,7 +134,7 @@ do
 /**DEBUT FONCTIONS**/
 
 //Procédure menuUtilisateur
-void menuUtilisateur(int *choix, int *m, int *n,int *nbK, int *min, int *max)
+void menuUtilisateur(int *choix, int *m, int *n,int *k, int *min, int *max)
 {
     printf("Choisissez ce que vous voulez faire : \n - 1 : Generation d une instance de type Im\n - 2 : Generation d une instance IprimeM\n - 3 : Generation d instances multiples\n\nVotre choix : ");
     scanf("%d", choix);
@@ -140,20 +143,29 @@ void menuUtilisateur(int *choix, int *m, int *n,int *nbK, int *min, int *max)
         switch(*choix)
         {
             case 1 ://Génération de l'instance Im
-                printf("Saisissez le nombre de machine m : ");
-                scanf("%d", m);
+                do
+                {
+                   printf("Saisissez le nombre de machine m : ");
+                   scanf("%d", m);
+                   if(*m>=300)
+                   {
+                       printf("Pour une instance Im, veuillez saisir un nombre de machine inferieur à 300. Sinon, temps trop long");
+                   }
+
+                }while(*m >= 300);
+
                 break;
             case 2 ://Génération de l'instance I'm
                 printf("Saisissez le nombre de machine m : ");
                 scanf("%d", m);
                 break;
-            case 3 ://Génération des instances aléatoires
+            case 3 ://Génération des instances IR
                 printf("Saisissez le nombre de machine m : ");
                 scanf("%d", m);
                 printf("Saisissez le nombre de taches n : ");
                 scanf("%d", n);
                 printf("Saisissez le nombre de d'instance k :");
-                scanf("%d", nbK);
+                scanf("%d", k);
                 //Mettre sécurité en fonction des contraintes
                 printf("Saisissez la duree minimum des taches :");
                 scanf("%d", min);
@@ -164,7 +176,7 @@ void menuUtilisateur(int *choix, int *m, int *n,int *nbK, int *min, int *max)
             default:
                 break;
         }
-    }while( !(*m > 0) && !(*n > 0) && !(*nbK > 0) && !(*max > 0) && !(*min < *max) );
+    }while( !(*m > 0) && !(*n > 0) && !(*k > 0) && !(*max > 0) && !(*min < *max));
 }
 
 void generationIm(int m, int *n,int *D)
@@ -212,7 +224,7 @@ void generationIR(int *D, int n, int min,int max)
 {
     int i;
 
-    for(i=0; i<n; i++)
+    for(i=0; i<n; i++)//On attribue des durées aléatoires à notre instance
     {
         D[i] = rand()%(max-min) + min;
     }
@@ -220,7 +232,7 @@ void generationIR(int *D, int n, int min,int max)
 
 int LSA(int m, int n, int *D, int *M,int *borneInferieurMoyenne)
 {
-    int i=0,y=0,z=0,machinex=0,tempsMin=0, numMachineSauv=0;
+    int i=0,y=0,tempsMin=0, numMachineSauv=0;
     int tempsTotal=0;
     *borneInferieurMoyenne = 0;
     tempsMin=m+m+1;
@@ -233,7 +245,7 @@ int LSA(int m, int n, int *D, int *M,int *borneInferieurMoyenne)
         for(y=0 ; y<m ;y++)
         {
             //Si on a une machine qui à un temps plus petit que les autres alors c'est cette machine là que l'on sélectionne
-            machinex = M[y];
+
 
             if(M[y] < tempsMin)
             {
@@ -257,9 +269,9 @@ int LSA(int m, int n, int *D, int *M,int *borneInferieurMoyenne)
         *borneInferieurMoyenne += D[i];
         //Notre machine obtien la durée de la tâche
 
-        z = D[i];
+
         M[numMachineSauv] += D[i];
-        machinex = M[numMachineSauv];
+
         //Si jamais cette machine à le temps le plus grand de toute les machines alors temps total sera égal au temps de cette machine
         if(M[numMachineSauv] > tempsTotal)
         {
@@ -276,7 +288,7 @@ int LSA(int m, int n, int *D, int *M,int *borneInferieurMoyenne)
 
 int LTP(int m, int n, int *D, int *M,int *borneInferieurMoyenne)
 {
-    int i,j,tmp,fini=0,tempsMax, valActuelle;
+    int i,j,tmp,tempsMax;
 
 //tri dans l'ordre décroissant
     for(i=0;i<n;i++)
@@ -298,12 +310,6 @@ int LTP(int m, int n, int *D, int *M,int *borneInferieurMoyenne)
 }
 
 
-
-void calculRatio()
-{
-
-}
-
 void initM(int *M,int n)
 {
     int i = 0;
@@ -311,16 +317,3 @@ void initM(int *M,int n)
     for(i = 0; i<n; i++)
     {M[i] = 0;}
 }
-
-
-/*
-void AffichageFinal(int borneInferieurMax,int borneInferieurMoyenne, int resultatLSA, int maxb, float)
-{
-    printf("BorneInferieurMax : %d\n", borneInferieurMax);
-    printf("BorneInferieurMoyenne : %d\n",borneInferieurMoyenne);
-    printf("Resultat LSA : %.0f\n",resultatLSA);
-    printf("maxb : %f\n",maxb);
-    printf("Ratio LSA : %.2f\n",ratioLSA);
-    printf("Resultat LTP : %.0f\n", resultatLTP);
-    printf("Ratio LTP : %.2f\n\n", ratioLTP);
-}*/
